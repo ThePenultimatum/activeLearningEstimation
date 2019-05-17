1d93920f1e0c71749ec582d94e49ee44b68603d3
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2019-05-17 12:35:24 -0500

    Maybe fixed part 3 of the kalman filter, editing graph now

--- hw3problem3.m
+++ hw3problem3.m
@@ -37,6 +37,8 @@ perturbedXhatsAll = [];
 perturbedXpredictionsAll = [];
 perturbedKksAll = [];
 
+originalTrajectoryValsAll = [];
+
 for m=1:M
     
     Ak = [0 1; -1 0];
@@ -47,7 +49,16 @@ for m=1:M
     C_k = [1 0; 0 1];
     R_k = [1 0; 0 1];
     
+    origxprev = [1; 1];
+    originalTrajectoryVals_t = [origxprev];
+    
     for i=1:(T/dt)
+        
+        % original trajectory
+        neworig = origxprev + dt * xdotWithOneXAndNoise(origxprev, [0, 0]);
+        originalTrajectoryVals_t(:,i+1) = origxprev + dt * xdotWithOneXAndNoise(origxprev, [0, 0]);
+        origxprev = neworig;
+        
         %%% Prediction phase
         % Calculate the noise to be used
         noise_t_w_v = noisesigmaval.*randn(2,2);% + b;
@@ -56,7 +67,7 @@ for m=1:M
         % calculate the prediction of x with the noise
         xhat_k_given_kminus1 = xhat_kminus1_given_kminus1 + dt * xdotWithOneXAndNoise(xhat_kminus1_given_kminus1, noise_t_w);
         xpredictions(:,i) = xhat_k_given_kminus1;
-        z_k = xhat_k_given_kminus1 + noise_t_v; % would also potentially have a measurement model by which to multiply newx
+        z_k = xhat_k_given_kminus1;% + noise_t_v; % would also potentially have a measurement model by which to multiply newx
         % Calculate the update to the prediction covariance
         P_k_given_kminus1 = Ak * P_kminus1_given_kminus1 * transpose(Ak); % + Q_k; % Q_k from noise, assume mean 0 so this can be skipped?
         pkkminus1s(:,:,i) = P_k_given_kminus1;
@@ -102,7 +113,7 @@ for m=1:M
         % derivative
         perturbed_K_k_optimal = perturbed_P_k_given_kminus1 * transpose((C_k) * inv(C_k * perturbed_P_k_given_kminus1 * transpose(C_k)) + R_k);
         % perturb kk
-        noise_kk_perturb = sqrt(0.1).*randn(2,2);
+        noise_kk_perturb = sqrt(0.01).*randn(2,2);
         perturbed_K_k = perturbed_K_k_optimal + noise_kk_perturb;
         perturbedKks(:,:,i) = perturbed_K_k;
         % Calculate P_k_given_k which becomes the p_kminus1_given_kminus1 for
@@ -137,14 +148,16 @@ for i=1:T/dt
     vals4(i) = pkkminus1s(2,2,i);
 end
 
-plot(xpredictionsAll(1,:),xpredictionsAll(2,:),".")
+plot(xpredictionsAll(1,:),xpredictionsAll(2,:));%,".")
 xlim([-10 10]); 
 ylim([-10 10]);
 title("Position");
 xlabel("x1");
 ylabel("x2");
 hold on;
-plot(perturbedXpredictionsAll(1,:),perturbedXpredictionsAll(2,:),".")
+plot(originalTrajectoryVals_t(1,:),originalTrajectoryVals_t(2,:));
+hold on;
+plot(perturbedXpredictionsAll(1,:),perturbedXpredictionsAll(2,:));%,".")
 hold off;
 
 
